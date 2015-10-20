14fd923656d936bdb9382118630df747601b8adb
jmillsss <jarettmiller@Jaretts-MBP.home>
2015-10-19 21:02:33 -0400

    declaring global vars
    for pcb & mem and tables

--- source/globals.ts
+++ source/globals.ts
@@ -31,13 +31,16 @@ var _OSclock: number = 0;  // Page 23.
 
 var _Mode: number = 0;     // (currently unused)  0 = Kernel Mode, 1 = User Mode.  See page 21.
 
-
+var _Memory:TSOS.Memory
+var _PCB: TSOS.PCB;
 
 
 var _UserProgIn;
 var _Canvas: HTMLCanvasElement;         // Initialized in Control.hostInit().
 var _DrawingContext: any; // = _Canvas.getContext("2d");  // Assigned here for type safety, but re-initialized in Control.hostInit() for OCD and logic.
 var _StatusBar;
+var CPUTbl: HTMLTableElement;
+var _MemoryTbl: HTMLTableElement;
 
 
 
